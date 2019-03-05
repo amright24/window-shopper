04a510d48d19d3db4721b090530fe38dc95aee84
Austin Rightnowar <austinrightnowar@Austins-MacBook-Pro.local>
2019-03-05 08:37:37 -0500

    added location and currency symbols

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable
 
 class CurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8175364848, green: 0.8175364848, blue: 0.8175364848, alpha: 0.8046607449)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
